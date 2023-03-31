28339f043e8e24862f43806b46a82ccc249a8ea2
lichao <184078718@qq.com>
2023-03-31 18:27:49 +0800

    发布

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cd docs/.vitepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-
+# 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
 git push -f git@github.com:MissSage/ms_istar.git master:gh-pages
 
 cd -
