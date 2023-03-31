432df0e0be9a06a11ae817c622b11f0c6dfaa956
lichao <184078718@qq.com>
2023-03-31 17:14:27 +0800

    打包

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,9 +1,10 @@
+import path from 'path'
 import {defineConfig} from 'vitepress'
 import { themeConfig } from './themeConfigs'
 export default defineConfig({
   lastUpdated: true,
   title: '自定义的 title',
-  outDir: '../dist',
+  outDir: path.resolve(__dirname,'dist'),
   // 在head中的meta中的描述信息
   description: '自定义的 description',
   head: [['meta', { name: 'keywords', content: 'HTML, CSS, JavaScript' }]],
